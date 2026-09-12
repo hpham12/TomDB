9bf3bc4ebe3346f7e1ba0cfac8b53997732c36c2
Hieu Pham <hieuminhphamcse21@gmail.com>
2026-09-12 14:55:44 -0400

    implemented file manager

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -1,20 +1,100 @@
 #include "file_manager.h"
+
+#include <cstring>
+#include <filesystem>
+
 #include "constants.h"
 #include "iostream"
+#include "page.h"
 
 //
 // Created by Hieu Pham on 8/30/26.
 //
-FileManager::FileManager(const std::string &filePath) {
-    file.open(filePath, std::fstream::in | std::fstream::out | std::fstream::app);
+FileManager::FileManager(std::filesystem::path &filePath) {
+    try {
+        // 1. Extract the directory portion and create it if missing
+        if (filePath.has_parent_path()) {
+            std::filesystem::create_directories(filePath.parent_path());
+        }
 
-    if (!file.is_open()) {
-        std::cerr << "Could not open file " << DATABASE_FILE_NAME << std::endl;
+        if (!std::filesystem::exists(filePath)) {
+            std::ofstream create(filePath);
+        }
+        filestream.open(filePath, std::fstream::in | std::fstream::out);
+
+        if (!filestream.is_open()) {
+            std::cerr << "Could not open file " << filePath << '\n';
+            exit(1);
+        }
+    } catch (const std::filesystem::filesystem_error &e) {
+        std::cerr << "Filesystem error: " << e.what() << '\n';
         exit(1);
     }
 
-    file.seekp (0, std::ios::end);
-    size_t fileSize = file.tellg();
+    filestream.seekg(0, std::ios::end);
+    size_t fileSize = filestream.tellg();
 
-    this->numPages = fileSize / PAGE_SIZE;
+    numPages = fileSize / PAGE_SIZE;
+    if (numPages == 0) {
+        extend();
+    }
+}
+
+std::unique_ptr<Page> FileManager::load(uint16_t pageId) {
+    if (pageId >= numPages) {
+        throw std::out_of_range("Requested page not exist");
+    }
+
+    size_t offset = pageId * PAGE_SIZE;
+    filestream.seekg(offset, std::ios::beg);
+    std::unique_ptr<Page> page = std::make_unique<Page>();
+    filestream.read(page->pageData.get(), PAGE_SIZE);
+
+    return page;
+}
+
+bool FileManager::flush(uint16_t pageId, const Page &page) {
+    if (pageId >= numPages) {
+        std::cerr << "Requested page not exist" << '\n';
+        return false;
+    }
+
+    size_t offset = pageId * PAGE_SIZE;
+    filestream.seekp(offset, std::ios::beg);
+    filestream.write(page.pageData.get(), PAGE_SIZE);
+    filestream.flush();
+
+    return true;
+}
+
+void FileManager::extend(size_t maxPageId) {
+    if (maxPageId < numPages) {
+        std::cerr << "maxPageId is smaller than the current number of pages" << '\n';
+        return;
+    }
+
+    size_t numPagesToAdd = maxPageId - numPages + 1;
+    size_t bufferSize = numPagesToAdd * PAGE_SIZE;
+    char buffer[bufferSize];
+
+    for (size_t i = 0; i < numPagesToAdd; i++) {
+        Page newPage;
+        memcpy(buffer + (i * PAGE_SIZE), newPage.pageData.get(), PAGE_SIZE);
+    }
+
+    size_t pageOffset = numPages * PAGE_SIZE;
+
+    filestream.seekp(pageOffset, std::ios::beg);
+    filestream.write(buffer, bufferSize);
+    filestream.flush();
+    numPages += numPagesToAdd;
+}
+
+void FileManager::extend() {
+    size_t pageOffset = numPages * PAGE_SIZE;
+    Page newPage;
+    filestream.seekp(pageOffset, std::ios::beg);
+    filestream.write(newPage.pageData.get(), PAGE_SIZE);
+    filestream.flush();
+    numPages++;
 }
